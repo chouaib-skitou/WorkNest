0d196f9f78bbab06fb7d18cded541885b4dbf382
Chouaib <chouaib.skt@gmail.com>
2025-03-02 13:35:28 +0100

    Add environment configuration for local identity and project services

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    identityServiceUrl: 'http://localhost:5000/api/auth', // Local identity service
+    projectServiceUrl: 'http://localhost:5001/api/projects' // Local project service
+  };
+  
