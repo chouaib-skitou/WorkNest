155b1fef966e3b5c578b07cffbb3c510534195ae
Chouaib <chouaib.skt@gmail.com>
2025-03-02 14:02:39 +0100

    Update local identity and project service URLs in environment configuration

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    identityServiceUrl: 'http://localhost:5000/api/auth', // Local identity service
-    projectServiceUrl: 'http://localhost:5001/api/projects' // Local project service
+    identityServiceUrl: 'http://localhost:5000/api', // Local identity service
+    projectServiceUrl: 'http://localhost:5001/api' // Local project service
   };
   
