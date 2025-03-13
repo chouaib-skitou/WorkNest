1283915c9442cf6b6418567948d942e7251177c9
Chouaib <chouaib.skt@gmail.com>
2025-03-13 09:39:07 +0100

    feat: remove console logs from JWT configuration for cleaner code

--- services/identity-service/config/jwt.js
+++ services/identity-service/config/jwt.js
@@ -4,23 +4,18 @@ import jwt from "jsonwebtoken";
 dotenv.config();
 
 export const generateToken = (user) => {
-  console.log("JWT_SECRET", process.env.JWT_SECRET);
   return jwt.sign({ id: user.id, role: user.role }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
 };
 
 export const generateRefreshToken = (user) => {
-  console.log("JWT_REFRESH_SECRET", process.env.JWT_REFRESH_SECRET);
   return jwt.sign({ id: user.id }, process.env.JWT_REFRESH_SECRET, {
     expiresIn: process.env.JWT_REFRESH_EXPIRES_IN,
   });
 };
 
 export const verifyToken = (token, isRefreshToken = false) => {
-  console.log("verifyToken --------------------------");
-  console.log("JWT_SECRET", process.env.JWT_SECRET);
-  console.log("JWT_REFRESH_SECRET", process.env.JWT_REFRESH_SECRET);
   const secret = isRefreshToken ? process.env.JWT_REFRESH_SECRET : process.env.JWT_SECRET;
     return jwt.verify(token, secret);
 };
